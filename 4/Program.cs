f44c65747d3666200e7109926c1a393ef017eb1b
D1SS <D1SS@DESKTOP-2QR7GQG>
2022-03-18 18:40:57 +0300

    Добавьте файлы проекта.

---
+++ 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            string myZodiakSign;
+            string myWork;
+
+
+            Console.WriteLine("Как Вас зовут");
+
+            myName = Console.ReadLine();
+
+            Console.WriteLine("Какой Ваш знак зодиака?");
+
+            myZodiakSign = Console.ReadLine();
+
+            Console.WriteLine("Где Вы работаете?");
+
+            myWork = Console.ReadLine();
+
+            Console.WriteLine("Вас зовут " + myName + ", ваш знак зодиака " + myZodiakSign + ", вы работаете в " + myWork + ".");
+        }
+    }
+}
